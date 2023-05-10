a678b543f16127a837c7ceb38be00a4d916255c5
Stakehouse <102478146+stakehouse-dev@users.noreply.github.com>
2023-05-10 07:03:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dcommon-constants
+
+This repo contains some important constants that are used in operations of Common Interest Protocol.
---
+++ index.js
@@ -0,0 +1,9 @@
+// Block number where crypto library was updated to version 2.x.x.
+// Major updates:
+// 1. DH key exchange for AES-CBC key formation
+// 2. NIZK formation and verification in terms of the order of parameter hashing
+const GOERLI_CRYPTOLIB_V2_BLOCK = 8924050;
+
+module.exports = {
+  GOERLI_CRYPTOLIB_V2_BLOCK,
+};
